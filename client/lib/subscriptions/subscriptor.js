6854f394d9cf93d8c47816a010f8d867e5f73364
sampathSL <sampath.thennakoon@gmail.com>
2015-03-03 00:09:52 +0530

    Initial Commit

---
+++ client/lib/subscriptions/subscriptor.js
@@ -0,0 +1,7 @@
+/**
+ * Created by test on 3/2/15.
+ */
+Meteor.subscribe('showItemCount');
+
+Meteor.subscribe('showClickCount');
+
---
+++ server/server.js
@@ -0,0 +1,28 @@
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    if(Items.find().count() == 0)  {
+        for(var i = 0; i < 10 ; i++){
+            Items.insert({"creationDate" : new Date() , "sentence" : " This is item " + i});
+        }
+    }
+  });
+}
+
+Meteor.methods({
+    'addClick' : function(){
+        Clicks.insert({"creationDate" : new Date() , "num" : Math.floor(Math.random() * 1000)});
+    },
+    'addItemClick' : function(){
+        Items.insert({"creationDate" : new Date() , "sentence" : " This is item " + (Items.find().count() + 1)});
+    }
+});
+
+Meteor.publish('showItemCount',function(){
+    return Items.find();
+});
+
+Meteor.publish('showClickCount',function(){
+    return Clicks.find();
+});
